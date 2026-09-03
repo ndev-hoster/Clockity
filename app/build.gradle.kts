4d8915ac27a71294d3a480baf4db71245e69b765
ndev-hoster <ndev.hoster@gmail.com>
2026-09-03 12:30:39 +0530

    Configure dedicated release keystore and remove unused overlay/keyguard permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,20 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("${rootDir}/keystore/clockity-release.jks")
+            storePassword = "clockity123"
+            keyAlias = "clockity"
+            keyPassword = "clockity123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
